738b5aab102f6974c85cc78f69be725c80de47a7
Digvijay Singh <Digvijay.Singh@BDINDIA.LOCAL>
2019-06-02 12:35:23 +0530

    updated quantity field

--- src/app/model/Product.go
+++ src/app/model/Product.go
@@ -4,7 +4,7 @@ type Product struct {
 	Model
 	Rating uint `json:"rating"`
 	Description string `json:"description"`
-
+	Quantity int `json:"quantity"`
 	MetaDescription string `json:"MetaDescription"`
 	Name string `json:"name"`
 	MetaKeyword string `json:"meta_keyword"`
